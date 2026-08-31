90215ae903ade78338572652da1e2c06f1d14f29
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-08-31 16:13:56 +0200

    Image fixed

--- Fithub/src/Pages/Frontpage/Frontpage.jsx
+++ Fithub/src/Pages/Frontpage/Frontpage.jsx
@@ -1,11 +1,12 @@
 import cover1 from "../../assets/cover1.jpg";
+import cover2 from "../../assets/cover2.jpg";
+import style from "./Frontpage.module.scss";
 
 export function Frontpage() {
   return (
     <>
-      <div>
-        <img src="cover1.jpg" alt="" />
-      </div>
+      <img className={style.pic1} src={cover2} alt="" />
+      <img src={cover1} alt="" />
     </>
   );
 }
