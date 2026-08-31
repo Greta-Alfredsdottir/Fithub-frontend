9853cd9485db7183b0fc2585f818ffcf651ba385
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-08-31 15:52:17 +0200

    problemmer med images

--- Fithub/src/Pages/Frontpage/Frontpage.jsx
+++ Fithub/src/Pages/Frontpage/Frontpage.jsx
@@ -1,10 +1,11 @@
 import cover1 from "../../assets/cover1.jpg";
-import { cover2 } from "../../assets/cover2.jpg";
 
 export function Frontpage() {
   return (
-    <div>
-      <img src="cover1.jpg" alt="" />
-    </div>
+    <>
+      <div>
+        <img src="cover1.jpg" alt="" />
+      </div>
+    </>
   );
 }
