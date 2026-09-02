9f18d35cebdaaeb5888b6a126f6034556198c9c2
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-09-02 14:38:50 +0200

    Homepage fixet, code gjort forståelig

--- Fithub/src/Pages/Homepage/Homepage.jsx
+++ Fithub/src/Pages/Homepage/Homepage.jsx
@@ -2,35 +2,23 @@
 // Lave en slider til classes
 // billeder på slider skal kunne klikkes på
 import { useFetch } from "../../Hooks/useFetch";
-import { useEffect } from "react";
+
 export function Homepage({ name }) {
   const { data, isLoading, error } = useFetch(
     import.meta.env.VITE_PUBLIC_BASE_URL + "/api/teams",
   );
-  // Bruger name til at finde et hold
-  const matchedTeam =
-    data?.find((item) => {
-      if (!name) return false;
-      return item?.name?.toLowerCase() === name?.toLowerCase();
-    }) || data?.[0];
-  // Henter en billede sti, fra den fundet team
-  const imageUrl = matchedTeam?.image?.url;
-  // Sammen sætter sti, med miljøvariablen
-  const fullImageUrl = imageUrl
-    ? import.meta.env.VITE_PUBLIC_BASE_URL + imageUrl
-    : null;
+  console.log(data);
 
   if (isLoading) return <p>Indlæser...</p>;
   if (error) return <p>Fejl ved hentning af data</p>;
-
+  // string "" med fuld Url + data, så ser man image
   return (
     <>
-      {fullImageUrl ? (
-        <img src={fullImageUrl} alt={matchedTeam?.name || "Team image"} />
-      ) : (
-        <p>Intet billede tilgængeligt</p>
-      )}
-      <h2>{matchedTeam?.name || "Intet hold valgt"}</h2>
+      <img
+        src={"http://localhost:3000" + data?.[0].image.url}
+        alt={"trainerimage"}
+      />
+      <h2>{data?.[0].name}</h2>
     </>
   );
 }
