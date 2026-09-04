54ee9882cda1afe85d6bc4f98b712934494587ff
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-09-04 07:43:26 +0200

    Still trying to make burgermenu

--- Fithub/src/Components/Navigation/Navigation.jsx
+++ Fithub/src/Components/Navigation/Navigation.jsx
@@ -1,20 +1,22 @@
 import { NavLink } from "react-router";
 import style from "./Navigation.module.scss";
 
-export const Navigation = () => {
+export function Navigation() {
   return (
-    <nav className={style.navStyle}>
-      <ul>
-        <li className={style.linkStyle}>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/Search">Search</NavLink>
-        </li>
-        <li>
-          <NavLink to="/Login">Login</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className={style.navStyle}>
+        <ul>
+          <li className={style.linkStyle}>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Search">Search</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Login">Login</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
-};
+}
